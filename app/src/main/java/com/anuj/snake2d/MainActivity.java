389ce8cc9bbe2ed9fd20034c70cc50f7a5a40d6b
Anuj Gupta <artishoepalace@gmail.com>
2026-09-12 10:18:16 +0300

    Fix Android compile access modifier for v1.0.0.4

--- app/src/main/java/com/anuj/snake2d/MainActivity.java
+++ app/src/main/java/com/anuj/snake2d/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onWindowFocusChanged(boolean hasFocus) {
+    public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) enableImmersiveMode();
     }
